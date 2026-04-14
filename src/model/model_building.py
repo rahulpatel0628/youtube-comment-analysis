3d30b8e54a20b6d8a50b47a590764298de66b6e9
Patel Rahul <rahulpatel16092005@gmail.com>
2026-04-14 12:22:29 +0530

    add model building part

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -1,3 +1,90 @@
 import pandas as pd
-df=pd.read_csv("data/interim/preprocessed_train.csv")
-print(df.sample(5))
+import yaml
+import logging
+import joblib
+from pathlib import Path
+from lightgbm import LGBMClassifier
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def load_data(path: str) -> pd.DataFrame:
+    try:
+        logging.info("Loading training data")
+        df = pd.read_csv(path)
+        return df
+    except Exception as e:
+        logging.error(f"Error loading data: {e}")
+        raise
+
+
+def load_params(path: str):
+    try:
+        logging.info("Loading parameters")
+        with open(path, "r") as f:
+            params = yaml.safe_load(f)
+        return params
+    except Exception as e:
+        logging.error(f"Error loading params: {e}")
+        raise
+
+
+def train_model(X, y, params):
+    try:
+        logging.info("Training LightGBM model")
+
+        model = LGBMClassifier(**params)
+        model.fit(X, y)
+
+        logging.info("Model training completed")
+        return model
+
+    except Exception as e:
+        logging.error(f"Error during training: {e}")
+        raise
+
+
+def save_model(model, path: Path):
+    try:
+        logging.info("Saving model")
+
+        path.parent.mkdir(parents=True, exist_ok=True)
+        joblib.dump(model, path)
+
+        logging.info("Model saved successfully")
+
+    except Exception as e:
+        logging.error(f"Error saving model: {e}")
+        raise
+
+
+def main():
+    try:
+        data_path = "data/processed/train_tfidf.csv"
+        params_path = "params.yaml"
+        model_path = Path("models/lgb_model.pkl")
+
+        data = load_data(data_path)
+
+        params = load_params(params_path)['model_building']
+
+        X = data.drop('sentiment', axis=1)
+        y = data['sentiment']
+
+        model = train_model(X, y, params)
+
+        save_model(model, model_path)
+
+        logging.info("Model training pipeline completed")
+
+    except Exception as e:
+        logging.critical(f"Pipeline failed: {e}")
+        raise
+
+
+if __name__ == "__main__":
+    main()
